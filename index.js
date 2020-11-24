66b88c8ddb13e5c7abaea10e6f680e6db337a1c4
edmundo papi <edmundopierre@yahoo.com>
2020-11-24 12:57:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,22 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let time_split = time.split(":")
+  let time_concat =time_split[0].concant(time_split[1])
+  let result = parseInt(time_concat,10)
+
+  if (result <1200)
+  {
+    return "Good Morning";
+  } 
+
+  else if (result > 1700) {
+    return "Good Evening";
+  }
+
+  else {
+    return "Good Afternoon";
+  }
+}
 /* Write your implementation of displayMessage() */
